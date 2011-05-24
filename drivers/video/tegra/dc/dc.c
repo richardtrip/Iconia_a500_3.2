66a0a20ef4380586c5b905e8a222d2deb8e94ceb
Min-wuk Lee <mlee@nvidia.com>
2011-05-24 19:50:17 +0900

    ARM: tegra: dc: fix mutex lock in reset worker
    unlock mutex if reset worker finds the display disabled.
    
    Bug 801463
    
    Reviewed-on: http://git-master/r/29898
    (cherry picked from commit 672459efb81140b496e3749f1b1b14dcf84af74d)
    
    Change-Id: I94254fca387cddcd58166757e46422167ed9262d
    Reviewed-on: http://git-master/r/32758
    Reviewed-by: Min-wuk Lee <mlee@nvidia.com>
    Tested-by: Min-wuk Lee <mlee@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Jonathan Mayo <jmayo@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -1478,7 +1478,7 @@ static void tegra_dc_reset_worker(struct work_struct *work)
 	mutex_lock(&dc->lock);
 
 	if (dc->enabled == false)
-		return;
+		goto unlock;
 
 	dc->enabled = false;
 
@@ -1523,6 +1523,7 @@ static void tegra_dc_reset_worker(struct work_struct *work)
 	_tegra_dc_controller_enable(dc);
 
 	dc->enabled = true;
+unlock:
 	mutex_unlock(&dc->lock);
 	mutex_unlock(&shared_lock);
 }
