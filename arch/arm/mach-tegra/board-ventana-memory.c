4bedd3a1d943aa772decba58f37377b5676233fb
richardtrip <richard.trip@gmail.com>
2011-09-07 08:58:13 +0200

    ARM: tegra: ventana: nvidia memory table updates to prevent corruption
    To avoid memory corruption when device is operating at full temperature
    QUSE_EXTRA should always be set to 0 for frequencies 150MHz and less.
    As extra protection change FBIO_CFG5 to remove the region where there
    is TriState on the DQS signals thus preventing false DQS pulses (and
    false reads).
    
    (cherry-picked from 2dc075be0e8495654a84a6bc6afa63408e141b02)
    
    Original author: James Wylder <james.wylder@motorola.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    
    Change-Id: I5c711343737edf972251006484ea84661106e0f9
    Reviewed-on: http://git-master/r/43401
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    
    Conflicts:
    
    	arch/arm/mach-tegra/board-ventana-memory.c

--- arch/arm/mach-tegra/board-ventana-memory.c
+++ arch/arm/mach-tegra/board-ventana-memory.c
@@ -66,8 +66,8 @@ static const struct tegra_emc_table ventana_emc_tables_elpida_8Gb_300Mhz[] = {
 			0x00000000,   /* ODT_WRITE */
 			0x00000000,   /* ODT_READ */
 			0x00000282,   /* FBIO_CFG5 */
-			0xa0ae04ae,   /* CFG_DIG_DLL */
-			0x00070000,   /* DLL_XFORM_DQS */
+			0xa06a04ae,   /* CFG_DIG_DLL */
+			0x0001f000,   /* DLL_XFORM_DQS */
 			0x00000000,   /* DLL_XFORM_QUSE */
 			0x00000000,   /* ZCAL_REF_CNT */
 			0x00000003,   /* ZCAL_WAIT_CNT */
@@ -117,8 +117,8 @@ static const struct tegra_emc_table ventana_emc_tables_elpida_8Gb_300Mhz[] = {
 			0x00000000,   /* ODT_WRITE */
 			0x00000000,   /* ODT_READ */
 			0x00000282,   /* FBIO_CFG5 */
-			0xa0ae04ae,   /* CFG_DIG_DLL */
-			0x00070000,   /* DLL_XFORM_DQS */
+			0xa06a04ae,   /* CFG_DIG_DLL */
+			0x0001f000,   /* DLL_XFORM_DQS */
 			0x00000000,   /* DLL_XFORM_QUSE */
 			0x00000000,   /* ZCAL_REF_CNT */
 			0x00000005,   /* ZCAL_WAIT_CNT */
@@ -168,8 +168,8 @@ static const struct tegra_emc_table ventana_emc_tables_elpida_8Gb_300Mhz[] = {
 			0x00000000,   /* ODT_WRITE */
 			0x00000000,   /* ODT_READ */
 			0x00000282,   /* FBIO_CFG5 */
-			0xa0ae04ae,   /* CFG_DIG_DLL */
-			0x00070000,   /* DLL_XFORM_DQS */
+			0xa06a04ae,   /* CFG_DIG_DLL */
+			0x0001f000,   /* DLL_XFORM_DQS */
 			0x00000000,   /* DLL_XFORM_QUSE */
 			0x00000000,   /* ZCAL_REF_CNT */
 			0x00000007,   /* ZCAL_WAIT_CNT */
@@ -219,8 +219,8 @@ static const struct tegra_emc_table ventana_emc_tables_elpida_8Gb_300Mhz[] = {
 			0x00000000,   /* ODT_WRITE */
 			0x00000000,   /* ODT_READ */
 			0x00000282,   /* FBIO_CFG5 */
-			0xa07c04ae,   /* CFG_DIG_DLL */
-			0x007dd510,   /* DLL_XFORM_DQS */
+			0xA04C04AE,   /* CFG_DIG_DLL */
+			0x007FC010,   /* DLL_XFORM_DQS */
 			0x00000000,   /* DLL_XFORM_QUSE */
 			0x00000000,   /* ZCAL_REF_CNT */
 			0x0000000e,   /* ZCAL_WAIT_CNT */
