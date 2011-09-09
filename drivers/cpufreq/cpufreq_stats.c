2dfc629d3c129f3b4ebc08079c80f29bf771005b
Your Name <richard.trip@gmail.com>
2011-09-09 17:12:12 +0200

    Revert "cpufreq bugfixes"
    This reverts commit ffe79c5b52a6710c99e354d37b050e1fc759f927.

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -296,13 +296,11 @@ static int cpufreq_stat_notifier_trans(struct notifier_block *nb,
 	old_index = stat->last_index;
 	new_index = freq_table_get_index(stat, freq->new);
 
-	/* We can't do stat->time_in_state[-1]= .. */
-	if (old_index == -1 || new_index == -1)
+	cpufreq_stats_update(freq->cpu);
+	if (old_index == new_index)
 		return 0;
 
-	cpufreq_stats_update(freq->cpu);
-
-	if (old_index == new_index)
+	if (old_index == -1 || new_index == -1)
 		return 0;
 
 	spin_lock(&cpufreq_stats_lock);
