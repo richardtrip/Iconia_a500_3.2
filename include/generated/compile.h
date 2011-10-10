4c742b3aa81753349c9a5adb0df139d4e2bc7c9d
morfic <morfic@gmail.com>
2011-10-09 21:45:53 -0500

    Clean up

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 10 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 SMP PREEMPT Tue Oct 4 22:18:00 CDT 2011"
-#define LINUX_COMPILE_TIME "22:18:00"
-#define LINUX_COMPILE_BY "morfic"
-#define LINUX_COMPILE_HOST "othris"
-#define LINUX_COMPILE_DOMAIN "zerorealm.org"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
